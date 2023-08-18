b61c578fdbd2d989e3dae6a8f56d8e3ba58c2ae6
0019964 <0019964@host.docker.internal>
2023-08-19 00:59:21 +0200

    Feat - Add: Creación proyecto inicial

---
+++ src/main/java/com/calculadora/MicroCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCalculadoraApplication.class, args);
+	}
+
+}
